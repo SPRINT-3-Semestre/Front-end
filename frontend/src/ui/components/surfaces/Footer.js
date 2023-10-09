5817b69f7da7b862268c9e7cb286549b5527a73f
wesleylimacosta <wesley.costa@sptech.school>
2023-10-09 09:42:07 -0300

    fix - Footer/responsividade

--- frontend/src/ui/components/surfaces/Footer.js
+++ frontend/src/ui/components/surfaces/Footer.js
@@ -4,11 +4,11 @@ function Footer() {
   return (
     <>
       <footer className={`row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top ${style.bg_color}`}>
-        <div className="col mb-3">
+        <div className="col-2 col-md-3 mb-3">
           <a href="/" className="d-flex align-items-center mb-3 link-dark text-decoration-none"></a>
         </div>
 
-        <div className="col mb-3">
+        <div className="col-2 col-md-3 mb-3">
           <h5>Email</h5>
           <ul className="nav flex-column">
             <li className="nav-item mb-2 text-muted">financeiro@editmatch.com</li>
@@ -16,7 +16,7 @@ function Footer() {
           </ul>
         </div>
 
-        <div className="col mb-3">
+        <div className="col-2 col-md-3 mb-3">
           <h5>Telefones</h5>
           <ul className="nav flex-column">
             <li className="nav-item mb-2 text-muted">(11) 4002-8922</li>
@@ -24,7 +24,7 @@ function Footer() {
           </ul>
         </div>
 
-        <div className="col mb-2">
+        <div className="col-2 col-3 mb-2">
           <h5>Whatsapp</h5>
           <ul className="nav flex-column">
             <Link to="https://contate.me/editmatch" target='_blank'>
