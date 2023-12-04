c4a2af40de0b8da2cce798322f7ae7e4f147fc47
VictorXdAugusto <victor.pereira@sptech.school>
2023-12-04 13:31:05 -0300

    implement back from front

--- frontend/src/ui/components/CardCart.jsx
+++ frontend/src/ui/components/CardCart.jsx
@@ -3,7 +3,7 @@ function CardCart( props ) {
         <table className="table table-bordered">
                     <thead>
                         <tr>
-                            <th>Produto</th>
+                            <th>Editor</th>
                             <th>Habilidades</th>
                             <th>Preço</th>
                             <th>Ações</th>
@@ -11,8 +11,8 @@ function CardCart( props ) {
                     </thead>
                     <tbody>
                         <tr>
-                            <td>{props.editorname}</td>
-                            <td>{props.hability}</td>
+                            <td>{props.name}</td>
+                            <td>{props.skills}</td>
                             <td>R$: {props.price}</td>
                             <td>
                                 <button className="btn btn-danger btn-sm">Remover</button>
--- frontend/src/ui/components/CardPerson.jsx
+++ frontend/src/ui/components/CardPerson.jsx
@@ -1,19 +1,36 @@
-import style from '../styles/CardPerson.module.css'
-import defaultImage from '../images/personicon.png'
+import React from 'react';
+import style from '../styles/CardPerson.module.css';
+import defaultImage from '../images/personicon.png';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom'; 
 
-const sendToCart = (customer) => {
-  // axios.post("http://localhost:8080/pedidos/adicionar", customer, {
-  //   headers: {
-  //     'Authorization': 'Bearer ' + sessionStorage.getItem('authToken'), 
-  //     'Content-Type': 'application/json',
-  // }
-  // })
+const CardPerson = (props) => {
 
-  alert('Pedido enviado com sucesso!')
-};
+  const navigate = useNavigate();
+  let userId = sessionStorage.getItem('userId'); 
+  let data = {
+    "valorTotal": 100,  
+    "clientFinal": 2,
+    "editor": 3 
+  };
 
-function CardPerson(props) {
+  const sendToCart = () => {
+    axios.post("http://localhost:8080/carts", data, {
+      headers: {
+        'Authorization': 'Bearer ' + sessionStorage.getItem('authToken'), 
+        'Content-Type': 'application/json',
+      }
+    })
+    .then((response) => {
+      alert('Pedido enviado com sucesso!');
+      navigate('/carrinho');
+    })
+    .catch((error) => {
+      console.error('Erro ao enviar pedido:', error);
+      alert('Erro ao enviar pedido. Por favor, tente novamente mais tarde.');
+    });
+  };
+
   return (
     <div className={style.cardPerson}>
       <a><img src={props.personImage ? props.personImage : defaultImage} alt="personIcon" /></a>
@@ -22,12 +39,12 @@ function CardPerson(props) {
       <p className={style.skills}><b>Habilidades</b></p>
       <ul>
         {
-          props.skills ? props.skills.map((skill) => <li>{skill}</li>) : <li>Nenhuma</li>  
+          props.skills ? props.skills.map((skill) => <li key={skill}>{skill}</li>) : <li>Nenhuma</li>  
         }
       </ul>
-      <button className={style.bttn} onClick={sendToCart}>Contratar</button>
+      <button className={style.bttn} onClick={() => sendToCart(props)}>Contratar</button>
     </div>
-  )
-}
+  );
+};
 
-export default CardPerson
+export default CardPerson;
